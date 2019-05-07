5893eb60c010b5bd49b41e21b97546515314621a
pinzhura <pinzhura@corp.sputnik.ru>
2019-05-07 04:45:32 +0300

    Рефакторинг

--- test_first.py
+++ test_first.py
@@ -1,5 +1,4 @@
 import allure
-import logging
 
 from selenium import webdriver
 
@@ -16,5 +15,5 @@ def test_open_ya():
                                  'div/div[1]/div[1]/h1/a').click()
 
     # with allure.step('Шаг 4'):
-    logging.info(f'URL - {wd.current_url}')
+
     assert wd.current_url != 'https://news.yandex.ru/?msid=1557147681.82138.140563.247752&mlid=1557147086.glob_225'
